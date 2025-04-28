d8f9b8f6b56590b9d15795a57e93ead85b1b41a3
Liam <liam.winterton.dev@gmail.com>
2025-04-28 22:13:34 +0000

    Set background to zero

--- lua/plugins/catppuccin.lua
+++ lua/plugins/catppuccin.lua
@@ -1,8 +1,13 @@
 return {
-	"catppuccin/nvim",
-	name = "catppuccin",
-	priority = 1000,
-	config = function()
-		vim.cmd.colorscheme "catppuccin"
-	end
+    "catppuccin/nvim",
+    name = "catppuccin",
+    priority = 1000,
+    config = function()
+        vim.cmd.colorscheme "catppuccin"
+
+        -- Set transparency of BG to none, so the terminals options are respected in ghostty
+        vim.api.nvim_command('hi Normal guibg=NONE ctermbg=NONE')
+        vim.api.nvim_command('hi NonText guibg=NONE ctermbg=NONE')
+        vim.api.nvim_command('hi EndOfBuffer guibg=NONE ctermbg=NONE')
+    end
 }
