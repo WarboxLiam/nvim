58a1c67980d13eb31faf1da4876066eace18e1a3
Liam <liam@warboxcreative.co.uk>
2025-12-09 14:13:52 +0000

    fix(conform): will run prettier OR biome

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -3,22 +3,42 @@ return {
 	config = function()
 		vim.env.XDG_RUNTIME_DIR = vim.fn.expand("~/.cache/prettierd")
 
-		require("conform").setup({
+		local conform = require("conform")
+
+		local function js_formatter(bufnr)
+			-- Check if prettier is available (respects the require_cwd/root_file logic below)
+			if conform.get_formatter_info("prettier", bufnr).available then
+				return { "prettier" }
+			else
+				-- If prettier config not found, run both Biome formatters
+				return { "biome-organize-imports", "biome" }
+			end
+		end
+
+		conform.setup({
 			formatters_by_ft = {
-				javascript = { "prettierd", "prettier", stop_after_first = true },
-				javascriptreact = { "prettierd", "prettier", stop_after_first = true },
-				typescript = { "prettierd", "prettier", stop_after_first = true },
-				typescriptreact = { "prettierd", "prettier", stop_after_first = true },
-				css = { "prettierd", "prettier", stop_after_first = true },
-				html = { "prettierd" },
-				json = { "prettierd" },
-				yaml = { "prettierd" },
-				markdown = { "prettierd" },
+				javascript = js_formatter,
+				javascriptreact = js_formatter,
+				typescript = js_formatter,
+				typescriptreact = js_formatter,
+				css = { "biome", "biome-organize-imports", stop_after_first = true },
+				html = { "biome", "prettierd", stop_after_first = true },
+				json = { "biome", "prettierd", stop_after_first = true },
+				yaml = { "prettierd", stop_after_first = true },
+				markdown = { "prettierd", stop_after_first = true },
 				lua = { "stylua" },
 			},
 			format_on_save = {
 				timeout_ms = 3000,
 				lsp_fallback = true,
+			},
+			formatters = {
+				prettier = {
+					require_cwd = true,
+					cwd = require("conform.util").root_file({
+						".prettierrc",
+					}),
+				},
 			},
 		})
 	end,
