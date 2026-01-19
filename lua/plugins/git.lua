372c13773dd564e6a7b3b36101b8a319b43a67f9
Liam <liam@warboxcreative.co.uk>
2026-01-19 10:31:10 +0000

    added vim-fugitive
    Using it to create this commit actually

--- lua/plugins/git.lua
+++ lua/plugins/git.lua
@@ -1,8 +1,18 @@
 return {
-	"lewis6991/gitsigns.nvim",
-	config = function()
-		require("gitsigns").setup({
-			word_diff = true
-		})
-	end,
+	{
+		"lewis6991/gitsigns.nvim",
+		config = function()
+			require("gitsigns").setup({
+				word_diff = true
+			})
+		end,
+	},
+	{
+		"tpope/vim-fugitive",
+		config = function()
+			-- Set some sensible commands for git, prefix with g for sensibility
+
+			vim.keymap.set("n", "<leader>gs", vim.cmd.Git) -- Git status
+		end,
+	}
 }
