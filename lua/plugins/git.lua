32d0e3beafffbd559d91702bd72a62a68c95df17
Liam <liam@warboxcreative.co.uk>
2026-01-19 11:13:56 +0000

    Added ability to open files in github with vim-rhubarb

--- lua/plugins/git.lua
+++ lua/plugins/git.lua
@@ -1,17 +1,16 @@
 return {
 	{
 		"lewis6991/gitsigns.nvim",
-		config = function()
-			require("gitsigns").setup({
-				word_diff = true
-			})
-		end,
 	},
 	{
 		"tpope/vim-fugitive",
 		keys = {
 			{ "<leader>gs", "<cmd>Git<cr>", desc = "Git status" },
 			{ "<leader>gm", "<cmd>Git mergetool<cr>", desc = "Git mergetool" },
+			{ "<leader>go", "<cmd>GBrowse<cr>", desc = "Open file in github" }
 		},
+	},
+	{
+		"tpope/vim-rhubarb",
 	}
 }
