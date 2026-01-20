8515cd0da7fa6c5d734450c8c7c3c594885f2993
Liam <liam@warboxcreative.co.uk>
2026-01-20 09:14:02 +0000

    added harpoon and keymaps

---
+++ lua/plugins/harpoon.lua
@@ -0,0 +1,49 @@
+return {
+	"ThePrimeagen/harpoon",
+	dependencies = { "nvim-lua/plenary.nvim" },
+	opts = {},
+	keys = {
+		{
+			"<C-e>",
+			function()
+				require("harpoon.ui").toggle_quick_menu()
+			end,
+			{ desc = "Harpoon - Toggle quick menu" },
+		},
+		{
+			"<C-M>",
+			function()
+				require("harpoon.mark").add_file()
+			end,
+			{ desc = "Harpoon - Mark file" },
+		},
+		{
+			"<C-H>",
+			function()
+				require("harpoon.ui").nav_file(1)
+			end,
+			{ desc = "Harpoon - Nav file 1" },
+		},
+		{
+			"<C-J>",
+			function()
+				require("harpoon.ui").nav_file(2)
+			end,
+			{ desc = "Harpoon - Nav file 2" },
+		},
+		{
+			"<C-K>",
+			function()
+				require("harpoon.ui").nav_file(3)
+			end,
+			{ desc = "Harpoon - Nav file 3" },
+		},
+		{
+			"<C-L>",
+			function()
+				require("harpoon.ui").nav_file(4)
+			end,
+			{ desc = "Harpoon - Nav file 4" },
+		},
+	},
+}
